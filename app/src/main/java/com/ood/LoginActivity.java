eb918ffd1ed9ec4b12661a29d3952d6cda6f98a0
wulimin <1466299085@qq.com>
2021-04-11 01:20:53 -0400

    add login UI

--- app/src/main/java/com/ood/LoginActivity.java
+++ app/src/main/java/com/ood/LoginActivity.java
@@ -1,16 +1,46 @@
 package com.ood;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+//import R;
+
+import com.ImageViewdemo.RoundImageView;
+import com.ood.MyAdapter;
+import com.ood.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private RoundImageView roundImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
+        btnLogin = (Button)findViewById(R.id.login_button);
+        roundImageView = (RoundImageView) findViewById(R.id.showImg);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.xiaohui);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
